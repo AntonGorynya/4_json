394a923af958f17acee1e4f6c0ece6bfab45d84b
Anton <antongorynya1@gmail.com>
2017-10-04 14:19:10 +0300

    simplified function
    delete one for

--- pprint_json.py
+++ pprint_json.py
@@ -2,34 +2,20 @@ import json
 from sys import argv
 
 filepath = argv[1]
-#print(filepath)
+
 
 def load_data(filepath):
     with open(filepath) as f:
         json_data = json.load(f)
-     #   print(json_data)
+  #      print(json_data)
     return json_data
 
-def pretty_print_json(json_data, deep = 1, comma = 0):
+def pretty_print_json(json_data, deep = 0, comma = 0):
+
     if isinstance(json_data, dict):
-        comma2 = len(json_data.keys())
-        print("\t" * (deep-1), "{")
-        for key,value in json_data.items():
-            print("\t"*deep,"\"{}\":".format(key), end = '')
-            if isinstance(value,list):
-                comma = print_list(deep,comma2,value)
-
-            if isinstance(value,dict):
-                comma=print_dict(deep,comma2, value)
-
-            if (isinstance(value,list) == False and isinstance(value,dict) == False):
-                print_value(comma2, deep+1, value)
-            comma2 = comma2-1
-
-        print_right_border(comma, deep - 1, bound_type='dict')
-
+        comma=print_dict(deep, comma, json_data)
     elif isinstance(json_data, list):
-        comma=print_list(deep, comma2, json_data)
+        comma=print_list(deep, comma, json_data)
     else:
         print_value(comma, deep, json_data)
 
