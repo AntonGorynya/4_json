7a7a2113ec3d0429c56e01c77c1a89405c410cc2
Anton <antongorynya1@gmail.com>
2017-10-06 13:03:08 +0300

    rename variable

--- pprint_json.py
+++ pprint_json.py
@@ -1,28 +1,30 @@
 #! /usr/bin/python3
-# -*- coding: utf-8 -*-
 
-from sys import argv
 import json
 import argparse
 
 
 def load_data(filepath):
-    with open(filepath, encoding='utf-8') as f:
-        json_data = json.load(f)
+    with open(filepath, encoding='utf-8') as input_file:
+        json_data = json.load(input_file)
     return json_data
 
 
-def pretty_print_json(json_data_in):
-    print(json.dumps(json_data_in, sort_keys=True, indent=4,
+def pretty_print_json(input_datat):
+    print(json.dumps(input_datat, sort_keys=True, indent=4,
                      ensure_ascii=False))
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description='--> Pretty json output <--')
-    parser.add_argument("path", nargs='+', help="path to json file")
+    parser.add_argument("path", nargs=1, help="path to json file")
     return parser
 
 
+'''
+C:/Users/g84086619/Documents/Devman/4_json-master/4_json-master/pprint_json.py ../new2.json   - выполняется успешно
+C:/Users/g84086619/Documents/Devman/4_json-master/4_json-master/pprint_json.py new2.json      - выполняется успешно
+'''
 if __name__ == '__main__':
     parser = create_parser()
     namespace = parser.parse_args()
